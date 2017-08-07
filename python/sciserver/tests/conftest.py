ee8f9dd6a2bb77c899a3b8eb78f5f6db78180325
Brian Cherinka <havok2063@hotmail.com>
2017-08-07 14:40:26 -0400

    adding tests for authentication and login

--- python/sciserver/tests/conftest.py
+++ python/sciserver/tests/conftest.py
@@ -6,17 +6,27 @@
 # @Author: Brian Cherinka
 # @Date:   2017-08-04 14:24:44
 # @Last modified by:   Brian Cherinka
-# @Last Modified time: 2017-08-07 13:52:38
+# @Last Modified time: 2017-08-07 14:35:05
 
 from __future__ import print_function, division, absolute_import
 import pytest
 from sciserver import authentication
 
 
+userinfo = [('***', '***')]
+
+
+@pytest.fixture(params=userinfo)
+def userdata(request):
+    ''' Fixture to loop over user info '''
+    user, password = request.param
+    return user, password
+
+
 @pytest.fixture(scope='session')
-def token():
-    login = '***'
-    password = '***'
+def token(userdata):
+    ''' Fixture to generate a token using auth '''
+    login, password = userdata
     token = authentication.login(login, password)
     yield token
     token = None
